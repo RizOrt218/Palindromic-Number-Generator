fb560570bb8ab3622b6240ab13fa657847a282e5
RizOrt218 <rizhiafalen@gmail.com>
2015-12-10 11:04:32 -1000

    still need work

--- Palindromic-Generator.js
+++ Palindromic-Generator.js
@@ -1,18 +1,18 @@
+var count = 0;
+var arrayMem = [];
+
 function palindromeNumberGenerator( value ){
-  var count = 0;
   var sum;
   var reverseSum = sum.split('').reverse().join('');
 
-//take sum and add reverse sum
-  function reverseCurrentValue( value ){
-    sum = value + reverseSum;
 
-    count++;
+  sum = value + reverseSum;
 
-    if( sum === reverseSum ){
-      return sum + "is a Palindrome Number";
+  count++;
+
+  if( sum === reverseSum ){
+    return sum + "is a Palindrome Number";
 
   }
-
 }
 palindromeNumberGenerator( 15 );
