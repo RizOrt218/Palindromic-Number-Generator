1483c2debbc37fc5a5bb1ee4d9f21888ce0bbb4e
RizOrt218 <rizhiafalen@gmail.com>
2015-12-10 10:10:57 -1000

    test2

--- Palindromic-Generator.js
+++ Palindromic-Generator.js
@@ -1,7 +1,18 @@
 function palindromeNumberGenerator( value ){
   var count = 0;
-  var reverseValue = value.reverse();
-  var sumOfTwoValue;
+  var sum;
+  var reverseSum = sum.split('').reverse().join('');
+
+//take sum and add reverse sum
+  function reverseCurrentValue( value ){
+    sum = value + reverseSum;
+
+    count++;
+
+    if( sum === reverseSum ){
+      return sum + "is a Palindrome Number";
+
+  }
 
 }
 palindromeNumberGenerator( 15 );
